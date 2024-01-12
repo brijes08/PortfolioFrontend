1c8f17e6b998e8391780ce290e6002b7ed888a3e
Brijesh Kumar <brijes08@gmail.com>
2024-01-12 22:47:08 +0530

    ee

--- src/components/About.js
+++ src/components/About.js
@@ -120,20 +120,20 @@ const About = () => {
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12 col-md-12 col-12 m-auto'>
-
-            <div className='row abt_profile'>
-              <div className='col-md-4' style={{ padding: "0" }}>
-                <div className='prfl_img'>
-                  <div className="editImgBtn">
-                    <form>
-                      <label>
-                        <input type="file" />
-                        <i className="fas fa-edit"></i>
-                      </label>
-                    </form>
-                  </div>
-                  <img src={!show ? image : userData.images} alt="UserImage" />
-                  {/* <ul>
+            <form className="updateFormAbout">
+              <div className="updateButtonAbout"><input type={!editBtn ? "btn" : "submit"} value={!editBtn ? "Edit" : "Save"}  onClick={!editBtn ? editBtnAbout : postData} /></div>
+              <div className='row abt_profile'>
+                <div className='col-md-4' style={{ padding: "0" }}>
+                  <div className='prfl_img'>
+                    {!editBtn ? "" : (
+                      <div className="editImgBtn">
+                        <label>
+                          {/* <input type="file" /> */}
+                          <i className="fas fa-edit"></i>
+                        </label>
+                      </div>)}
+                    <img src={!show ? image : userData.images} alt="UserImage" />
+                    {/* <ul>
                     <li>
                       <a href="https://www.instagram.com/brijes08" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram" aria-hidden="true"></i> Instagram</a>
                     </li>
@@ -150,30 +150,30 @@ const About = () => {
                       <a href="https://github.com/brijes08" target="_blank" rel="noopener noreferrer"><i className="fab fa-github" aria-hidden="true"></i> Github</a>
                     </li>
                   </ul> */}
+                  </div>
                 </div>
-              </div>
-              <div className='col-md-8'>
-                <div className='prfl_cont' >
-                  <h2>Hello {!show ? "User Name" : userData.name}</h2>
-                  <span>I'm Happy to Hear That You are {!show ? "User Designation" : userData.work}</span>
-                  <p>Welcome to my world! I've glad you have decided to join me.</p>
-                  <p>I want to make your onboarding experience free of worry. Feel free to send me an email if you have any questions at any point in time. I also recommend checking out these resources to get you off the ground.</p>
-                </div>
-                <div className='pfl_dtls'>
-                  <h3 className="frUpdtFromBtnAbout">Your Details Are <div className="updateButtonAbout" onClick={editBtnAbout}>Edit <i className="fas fa-edit"></i></div></h3>
-                  <form className="updateFormAbout">
+                <div className='col-md-8'>
+                  <div className='prfl_cont' >
+                    <h2>Hello {!show ? "User Name" : userData.name}</h2>
+                    <span>I'm Happy to Hear That You are {!show ? "User Designation" : userData.work}</span>
+                    <p>Welcome to my world! I've glad you have decided to join me.</p>
+                    <p>I want to make your onboarding experience free of worry. Feel free to send me an email if you have any questions at any point in time. I also recommend checking out these resources to get you off the ground.</p>
+                  </div>
+                  <div className='pfl_dtls'>
+                    <h3 className="frUpdtFromBtnAbout">Your Details Are </h3>
+
                     <ul>
                       <li><p>Your User ID is</p><b>{!show ? "User ID" : userData._id}</b></li>
                       <li className='nameAbout'><p>Name</p><b>{!editBtn ? !show ? "User Name" : userData.name : <input type="text" name="name" value={userData.name} onChange={handleInput} required />}</b></li>
                       <li className='nameAbout'><p>Designation</p><b>{!editBtn ? !show ? "User Designation" : userData.work : <input type="text" name="work" value={userData.work} onChange={handleInput} required />}</b></li>
                       <li className='emailAbout'><p>Email</p><b>{!editBtn ? !show ? "User Email" : userData.email : <input type="email" name="email" value={userData.email} onChange={handleInput} required />}</b></li>
                       <li><p>Phone</p><b>{!editBtn ? !show ? "User Mobile Number" : userData.phone : <input type="tel" name="phone" value={userData.phone} onChange={handleInput} pattern="[1-9]{1}[0-9]{9}" minLength="10" maxLength="10" required />}</b></li>
-                      {!editBtn ? "" : <li><div></div><input type="submit" value="Update" onClick={postData} /></li>}
+                      {/* {!editBtn ? "" : <li><div></div><input type="submit" value="Save" /></li>} */}
                     </ul>
-                  </form>
+                  </div>
                 </div>
               </div>
-            </div>
+            </form>
 
           </div>
         </div>
