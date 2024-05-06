ff8b4af1875ab8b6706ce9613ad7f21a55b03eaf
Brijesh Kumar <brijes08@gmail.com>
2024-05-06 16:45:23 +0530

    sd

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 import pageBanner from '../images/page-banner.jpg'
 // import image from "../images/user.png"
 // import { useNavigate } from 'react-router-dom';
-import brijesh from '../images/Brijesh.JPG'
+import brijesh from '../images/brijesh.webp'
 import Resume from '../images/brijesh-cv.pdf'
 import TypingEffect from './TypingEffect';
 
